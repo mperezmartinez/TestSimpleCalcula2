31c4bbeea3b4696dae221605a5d00cd9b1cb1e64
Miguel <Miguel@Mipema>
2015-06-01 10:25:44 +0200

    TestSimple

--- test/calcula2/Calcula2Test.java
+++ test/calcula2/Calcula2Test.java
@@ -5,57 +5,53 @@
  */
 package calcula2;
 
-import java.util.Arrays;
-import java.util.Collection;
- 
-import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-import org.junit.runner.RunWith;
-import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-@RunWith(Parameterized.class)
+/**
+ *
+ * @author Miguel
+ */
 public class Calcula2Test {
-   private float inputNumber;
-   private float inputNumber2;
-   private int opc;
-   private float expectedRes;
-   private Calcula2 Calcula2Test;
+    
+    public Calcula2Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
 
-   @Before
-   public void initialize() {
-      Calcula2Test = new Calcula2();
-   }
-
-   // Each parameter should be placed as an argument here
-   // Every time runner triggers, it will pass the arguments
-   // from parameters we defined in primeNumbers() method
-   public Calcula2Test(float inputNumber,float inputnumber2 , Integer opc ,float expectedRes) {
-      this.inputNumber = inputNumber;
-      this.inputNumber2=inputnumber2;
-      this.opc = opc;
-      this.expectedRes=expectedRes;
-   }
-
-   @Parameterized.Parameters
-   public static Collection Calcula2Test() {
-      return Arrays.asList(new Object[][] {
-         {2,2,1,4},
-         { 4,2,1,6},
-         { 8,4,2,4},
-         { 2,4,3,8},
-         { 9,3,4,3}
-      });
-   }
 
     /**
      * Test of realizaOperacion method, of class Calcula2.
      */
     @Test
     public void testRealizaOperacion() {
-     System.out.println("Parameterized Number is : " + inputNumber);
-      assertEquals(expectedRes, Calcula2.realizaOperacion(inputNumber, inputNumber2, opc),0);
+        System.out.println("realizaOperacion");
+        float num1 = 2.0F;
+        float num2 = 3.0F;
+        int oper = 1;
+        float expResult = 5.0F;
+        float result = Calcula2.realizaOperacion(num1, num2, oper);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
     }
     
 }
